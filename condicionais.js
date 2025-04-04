a070785df8d7aae80895c9eda8a3b397ece29c55
apolinario <apolinariocruz.f@gmail.com>
2025-04-03 21:52:53 -0300

    tarefa

---
+++ condicionais.js
@@ -0,0 +1,11 @@
+let numero = prompt("digite o numero")
+
+if (numero > 0) {
+    console.log("positivo")
+}
+else {
+
+    if (numero < 0) {
+        console.log("negativo")
+    }
+}
--- script.js
+++ script.js
@@ -1,3 +1,33 @@
-alert (" Hello World Na Tela");
+// alert (" Hello World Na Tela");
 
-console.log ("Hello Word No Console");
+// console.log ("Hello Word No Console");
+let nome = "Fernando";
+
+const idade = 22;
+
+var cidade = "sáo paulo"
+
+console.log(nome, idade, cidade)
+console.log(idade)
+console.log(cidade)
+
+console.log("meu nome e: " + nome) // cocatenação
+
+console.log(`meu nome e: ${nome}`) // interpolação
+
+console.log("meu nome e: " + nome)
+
+console.log(` minha idade e: ${idade}`)
+
+console.log(`minha cidade e: ${cidade}`)
+
+let texto = "tenho 2 celulares"
+let number = "20"
+
+console.log(typeof cidade)
+
+let n1 = 10;
+let n2 = 10;
+
+let soma = n1 + n2;
+console.log(soma)
