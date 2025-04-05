76d18a82baabbe3ac777e2291f3717ce8587cb65
apolinario <apolinariocruz.f@gmail.com>
2025-04-04 21:49:49 -0300

    segunda

--- condicionais.js
+++ condicionais.js
@@ -1,11 +1,77 @@
-let numero = prompt("digite o numero")
+function exercio1() {
+    let numero = prompt("digite o numero")
 
-if (numero > 0) {
-    console.log("positivo")
-}
-else {
-
-    if (numero < 0) {
-        console.log("negativo")
+    if (numero > 0) {
+        alert("positivo")
     }
+    else {
+
+        if (numero < 0) {
+
+            alert("negativo")
+        }
+        if (numero == 0) {
+            alert(" e zero")
+        }
+    }
+}
+
+
+function exercio2() {
+
+    debugger;
+
+    let idade = prompt("digite sua idade")
+
+    if (" idade >= 18") {
+
+        alert(" voce e maior de idade")
+    } else {
+        alert("voce e menor de idade")
+    }
+
+}
+
+function exerci03() {
+
+    let numero = prompt("digite sua nota");
+
+    if (numero >= 60) {
+
+        alert("aprovado");
+
+    } else {
+
+        alert("reprovado");
+
+    }
+
+}
+function exerci04() {
+
+    let numero = prompt("digite o numero")
+
+    if (numero > 0) {
+        alert("positivo")
+
+    } else {
+        if (numero < 0) {
+            alert("negativo")
+        } else {
+            if (numero == 0) {
+                alert("seu numero e zero")
+            }
+
+        }
+    }
+
+} 
+function exercio5(){
+
+    let idade = prompt("digita sua idade")
+    if(idade >=0 && idade <= 12){
+        alert("criança")
+    }else if (idade >= 13 && idade <=17)
+        alert("adolecente")
+        
 }
