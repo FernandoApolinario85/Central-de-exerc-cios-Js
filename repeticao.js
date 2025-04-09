3a0f3216d90491f5a7f679dcf000d1afaf050856
apolinario <apolinariocruz.f@gmail.com>
2025-04-08 21:47:12 -0300

    intermediario

--- repeticao.js
+++ repeticao.js
@@ -5,11 +5,11 @@ function exemplofor() {
         alert(contador);
     }
 
-}    
+}
 function exemplowhile() {
 
     let contador = 1;
-    while (contador <= 5){
+    while (contador <= 5) {
 
         console.log(contador);
         contador++
@@ -17,12 +17,51 @@ function exemplowhile() {
 
     }
 }
-function exemplodowilhe(){
-debugger
-    let contador =1;
+function exemplodowilhe() {
+
+    let contador = 1;
     do {
         alert(contador);
         conatdor++;
-    }while (contador <=5 );
+    } while (contador <= 5);
 }
-exemplodowilhe()
+
+
+function basico1() {
+    debugger
+
+    for (let contador = 1; contador <= 10; contador++) {
+
+        console.log(contador);
+
+    }
+
+
+
+}
+
+function basico2() {
+    let valor = prompt("digite o valor");
+    for (let contador = 1; contador <= 10; contador++) {
+        alert(contador * valor);
+    }
+}
+
+function basico3() { //contado de 2 em 2
+    let valor = prompt("digite o valor");
+    for (let contador = 2; contador <= 50; contador += 2) {
+        alert(contador)
+    }
+}
+
+function intermediario() {
+
+    let valor = prompt("digite o valor")
+    while (valor >= 0) {
+
+        console.log(valor)
+        valor--
+    }
+}
+
+intermediario()
---
+++ teste.js
@@ -0,0 +1,13 @@
+// let valor = prompt("digite o valor");
+
+// for (let contador = 1; contador <= 10; contador++) {
+
+//     alert(contador * valor);
+// }
+
+let numero = prompt("digite seu numero")
+ let soma = 0
+ for ( let contador = 1; contador <= numero; contador++){
+    soma += contador;
+ }
+ console.log(`${soma}`)
