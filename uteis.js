7455af9e569a96806598cdd58c5468d6b3d847d8
apolinario <apolinariocruz.f@gmail.com>
2025-04-09 21:54:58 -0300

    utis

---
+++ uteis.js
@@ -0,0 +1,7 @@
+function exemploDeFilter() {
+
+    let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    let NumerosFiltrados = numeros.filter(numero => numero >2);
+    console.log(NumerosFiltrados)
+}exemploDeFilter()
